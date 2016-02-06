2df509ea8f193b34e78a511d0a7827022211e8fe
Corey Mason <corey.mason98@googlemail.com>
2016-02-05 18:53:12 -0800

    PID Shooter and Relay Intake
    Intake forward does not work while running shooter. Intake code needs to
    be moved to the Intake subsystem.

---
+++ robot/src/org/usfirst/frc3482/robot/commands/FeedShooter.java
@@ -0,0 +1,51 @@
+package org.usfirst.frc3482.robot.commands;
+
+import org.usfirst.frc3482.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class FeedShooter extends Command {
+	
+	boolean direction;
+    public FeedShooter() {
+        // Use requires() here to declare subsystem dependencies
+        requires(Robot.chassis);
+        requires(Robot.shooter);
+        direction = true;
+    }
+    
+    public FeedShooter(boolean direction) {
+    	this.direction = direction;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	if (direction) 
+    		Robot.shooter.startShooterFeed();
+    	else
+    		Robot.shooter.reverseShooterFeed();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.shooter.stopShooterFeed();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
--- robot/src/org/usfirst/frc3482/robot/commands/SpinShooter.java
+++ robot/src/org/usfirst/frc3482/robot/commands/SpinShooter.java
@@ -44,7 +44,7 @@ public class SpinShooter extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.shooter.startSpinning(0.9);
+    	Robot.shooter.spin();
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -54,8 +54,8 @@ public class SpinShooter extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.shooter.stopSpinning();
     	//finished = true;
+    	Robot.shooter.stopSpin();
     }
 
     // Called when another command which requires one or more of the same
