ea92ee49dccb9d405a4c9921897bb2d940cb0d85
Corey Mason <corey.mason98@googlemail.com>
2016-03-12 23:46:18 -0800

    Start Vision

--- robot/src/org/usfirst/frc3482/robot/subsystems/Camera.java
+++ robot/src/org/usfirst/frc3482/robot/subsystems/Camera.java
@@ -12,7 +12,15 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Camera extends Subsystem {
 
-	CameraServer server;
+	private CameraServer server;
+	private double tilt = Math.toRadians(31);
+	private double hT = 226.06;
+	private double hc = 29.21;
+	private double wc = 10;
+	private double FOVx = 640;
+	private double FOVy = 480;
+	private double view = Math.toRadians(24.5);
+	private double h = hT-hc;
 	
     public Camera() {
     	server = CameraServer.getInstance();
@@ -20,8 +28,23 @@ public class Camera extends Subsystem {
         //the camera name (ex "cam0") can be found through the roborio web interface
         server.startAutomaticCapture("cam0");
     }
-
-
+    /*
+    public double targetAngleOffset() {
+    	int wp = (int) Math.round(FOVx/2-Robot.centerX);
+    	double angle = Math.atan(2*wp*Math.tan(FOVx)/FOVx);
+    	double cameraToShooterAngleOffset = Math.atan(wc/12)/targetDistance();
+    	return Math.toDegrees(angle+cameraToShooterAngleOffset);
+    }
+    
+    public double targetDistance() {
+    	int hp = (int) Math.round(FOVy/2-Robot.centerY);
+    	if(hp == 0) {
+    		return h/Math.tan(tilt); 
+    	}
+    	double fov_adjustment = (FOVy*Math.cos(tilt)/2/hp/Math.tan(view))-Math.sin(tilt);
+    	return h*fov_adjustment/(1+fov_adjustment*Math.tan(tilt));
+    }
+*/
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
